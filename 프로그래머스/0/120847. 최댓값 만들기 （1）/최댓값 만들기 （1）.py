a9eef3d0aae3f1c6acfc7c4922fa3db020d3fac6
JOOJINHYEOK <jjh68979@gmail.com>
2025-09-15 13:10:18 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,6 +1,5 @@
 def solution(numbers):
     sort_numbers = sorted(numbers, reverse=True)
-    # print(sort_numbers)
-    # numbers.sort()
-    
+    # print(sort_numbers) -> 내림차순 정렬
+    # numbers.sort() -> 오름차순 정렬
     return sort_numbers[0] * sort_numbers[1]
