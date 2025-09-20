bb1ee72caf73e68917a32aff61637405d09f9ada
JOOJINHYEOK <jjh68979@gmail.com>
2025-09-20 20:40:56 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,10 +1,15 @@
 def solution(s1, s2):
     answer = 0
-    s1.sort()
-    s2.sort()
+#     s1.sort()
+#     s2.sort()
     
+#     for i in s1:
+#         for j in s2:
+#             if i == j:
+#                 answer += 1
+
+
     for i in s1:
-        for j in s2:
-            if i == j:
-                answer += 1
+        if i in s2:
+            answer += 1
     return answer
