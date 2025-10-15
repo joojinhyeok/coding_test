c7254fa26993dcdf5d4ac0397bd98e13fa69c277
JOOJINHYEOK <jjh68979@gmail.com>
2025-10-15 23:16:34 +0900

    [level 0] Title: 커피 심부름, Time: 0.75 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,9 @@
+def solution(order):
+    answer = 0
+    for i in order:
+        print(i)
+        if 'americano' in i or i == 'anything':
+            answer += 4500
+        elif 'cafelatte' in i:
+            answer += 5000
+    return answer
