6e09c60e27fcef939eac6d48fce8c5c95ba5d4ec
JOOJINHYEOK <jjh68979@gmail.com>
2026-06-16 15:51:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,8 +1,6 @@
 def solution(numbers):
-    i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    
-    for j in numbers:
-        if j in i:
-            i.remove(j)
-    answer = sum(i)
+    answer = 0
+    for i in range(10):
+        if i not in numbers:
+            answer += i
     return answer
