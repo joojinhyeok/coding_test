712f72312f74e786ca4c920b8fbd215cda4feb4b
JOOJINHYEOK <jjh68979@gmail.com>
2026-06-18 11:46:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,5 +1,2 @@
 def solution(phone_number):
-    p_list = list(phone_number)
-    for i in range(len(phone_number) - 4):
-        p_list[i] = '*'
-    return "".join(p_list)
+    return '*' * (len(phone_number) - 4) + phone_number[-4:]
