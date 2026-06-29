14b60239b705a9e6b521662caf9d27056cafb6b3
JOOJINHYEOK <jjh68979@gmail.com>
2026-06-30 08:52:06 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.py
@@ -1,7 +1,3 @@
 def solution(s):
-    # 1. 문자열 s를 내림차순 정렬 (결과는 리스트 형태 ['g', 'f', 'e', ...])
-    sorted_list = sorted(s, reverse=True)
-    
-    answer = "".join(sorted_list)
-    
-    return answer
+    # ASCII 순서
+    return ''.join(sorted(s, reverse=True))
